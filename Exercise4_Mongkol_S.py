83a6da92ec2aa77087f965a656603a06aa8ccfcb
TONmongkol <mongkolsahad@gmail.com>
2023-04-16 18:54:37 +0700

    Create Exercise4_Mongkol_S.py
    Exercise4

---
+++ Exercise4_Mongkol_S.py
@@ -0,0 +1,15 @@
+'''FE = Foundation English
+   GB = "General Business
+   ICS = "Introduction to Computer System 
+   Pro = "Computer Programming '''
+
+FE = 56
+GB = 72
+ICS = 49
+Pro = 66
+
+print(FE)
+print(GB)
+print(ICS)
+print(Pro)
+
